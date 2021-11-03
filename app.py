05d1d249d07169bc448601457d21a05f21d05365
ramansat87 <36228424+ramansat87@users.noreply.github.com>
2021-11-03 17:22:45 +0530

    first commit

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World, Welcome to the Azure DevOps and this is a sample Python Web App running on Flask Framework!"
+    return "Hello World Welcome to Azure Python World, Welcome to the Azure DevOps and this is a sample Python Web App running on Flask Framework!"
 
